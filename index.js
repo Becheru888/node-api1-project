566459ecc145e11e87f2b8b2db0acf5fc986921e
Becheru888 <becheru93@gmail.com>
2020-01-12 21:10:17 +0000

    find, findById, insert DONE

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // implement your API here
+const express = require("express");
+const DB = require("./data/db");
+
+const server = express();
+server.use(express.json());
+
+const port = 1000;
+
+server.get("/users", (req, res) => {
+  DB.find()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get("/user/:id", (req, res) => {
+
+  const userId = req.params.id;
+
+  DB.findById(userId).then(user => {
+    if (Number(userId) === user.id) {
+      res.status(200).json(user);
+    }
+  }).catch(error => {
+      res.status(400).json({error: 'User not found'})
+  });
+});
+
+server.post('/addUser', (req, res) => {
+    const newUser = req.body
+    DB.insert(newUser).then(data => {
+      res.status(200).json(data)
+    })
+})
+
+server.put('/editUser/:id', (res, req) => {
+
+})
+
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
